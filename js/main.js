5eea22d88aafdb2d384a1ab274ff32b30699d8a2
Aradier <lautaroad@gmail.com>
2017-12-09 15:09:39 -0500

    update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,21 @@ $('.hbtn').click(function(){
 $('.btnrsp').click(function(){
 	$('.tapamenuresponsive').fadeOut();
 })
+
+$(window).scroll(function(){
+	if ($(window).scrollTop()==0) {
+		$('.header').removeClass('headerfixed');
+		$('.prolificlogofixed').hide();
+		$('.prolificlogo').show();
+		$('.normalli').removeClass('navlistliafixed');
+		$('.lastli-menu').removeClass('lastli-menufixed');
+	}
+	else
+	{
+		$('.header').addClass('headerfixed');
+		$('.prolificlogofixed').show();
+		$('.prolificlogo').hide();
+		$('.normalli').addClass('navlistliafixed');
+		$('.lastli-menu').addClass('lastli-menufixed');
+	}
+})
